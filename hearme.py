604115815f6f6f4027b53a513ee5bae10e536fb8
Raymond Blum <raygeeknyc@gmail.com>
2017-05-13 17:19:45 -0400

    Updated to use streaming audio

--- hearme.py
+++ hearme.py
@@ -12,16 +12,17 @@ speech_client = speech.Client()
 
 # Load the sound into memory from the file named by the first parameter
 with io.open(sys.argv[1], 'rb') as audio_file:
-    content = audio_file.read()
+    audio_sample = speech_client.sample(
+        stream=audio_file,
+        encoding=speech.encoding.Encoding.LINEAR16,
+        sample_rate_hertz=16000)
 
-audio_sample = speech_client.sample(
-    content=content,
-    source_uri=None,
-    encoding='LINEAR16',
-    sample_rate_hertz=16000)
+    # Find transcriptions of the audio file
+    alternatives = audio_sample.streaming_recognize('en-US')
 
-# Find transcriptions of the audio file
-alternatives = audio_sample.recognize('en-US')
-print "Found %d alternative transcripts:" % len(alternatives)
-for alternative in alternatives:
-    print('Transcript: {}'.format(alternative.transcript))
+    #print "Found %d alternative transcripts:" % len(alternatives)
+    for alternative in alternatives:
+        print('Finished: {}'.format(alternative.is_final))
+        print('Stability: {}'.format(alternative.stability))
+        print('Confidence: {}'.format(alternative.confidence))
+        print('Transcript: {}'.format(alternative.transcript))
