064f964d693c513f320af4bb77228adf4c23ecda
Raymond Blum <raygeeknyc@gmail.com>
2017-07-01 11:36:33 -0400

    Updated to new setup_auth

--- setup_auth.sh
+++ setup_auth.sh
@@ -1,2 +1,10 @@
-#!/bin/sh
-gcloud auth application-default login
+#!/bin/bash
+echo "authenticating"
+GOOGLE_APPLICATION_CREDENTIALS=../paidtech-07060b047c89.json
+if [[ ! -r "$GOOGLE_APPLICATION_CREDENTIALS" ]]; then
+  echo "falling back to interactive authentication"
+  gcloud auth application-default login
+else
+  export GOOGLE_APPLICATION_CREDENTIALS
+fi
+echo "authenticated"
