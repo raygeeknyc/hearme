8726d95e665767fe81752f77cdcfd64ad8ec69e6
Raymond Blum <raygeeknyc@gmail.com>
2017-08-11 22:02:29 -0400

    New auto settings

--- setup_auth.sh
+++ setup_auth.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 echo "authenticating"
-GOOGLE_APPLICATION_CREDENTIALS=../paidtech-07060b047c89.json
+GOOGLE_APPLICATION_CREDENTIALS=../ohgee-923e3db09269.json
 if [[ ! -r "$GOOGLE_APPLICATION_CREDENTIALS" ]]; then
   echo "falling back to interactive authentication"
   gcloud auth application-default login
 else
   export GOOGLE_APPLICATION_CREDENTIALS
+  export GOOGLE_CLOUD_PROJECT=ohgee-176600
 fi
 echo "authenticated"
